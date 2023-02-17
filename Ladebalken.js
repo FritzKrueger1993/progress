869d51bd3da7baa11eb49fd4d0c62a122ecff259
FritzKrueger1993 <109686983+FritzKrueger1993@users.noreply.github.com>
2023-02-17 23:52:25 +0100

    Update Ladebalken.js

--- Ladebalken.js
+++ Ladebalken.js
@@ -57,7 +57,7 @@ function draw() {
   textAlign(RIGHT);
   textSize(20);
   text(nfc(prog,7), midX + barX *0.5 , midY + 46);
-  text(windowWidth,midX,10);
+  text('test',midX,10);
   text(displayHeight,midX,40);
   
 }
